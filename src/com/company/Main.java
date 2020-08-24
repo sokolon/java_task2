7d53b4d601879f39bc0f28e5ad8327061484e70c
Natalia <sokolowskan16@gmail.com>
2020-08-24 13:17:06 +0200

    input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        Scanner lineScanner = new Scanner(scanner.nextLine());
+        List<Integer> list = new ArrayList<>();
+        while (lineScanner.hasNextInt())
+        {
+            list.add(lineScanner.nextInt());
+        }
+
+        
     }
 }
