c8cbb48515f7f9aaa4be1f695ab8cfd04b8630b0
Natalia <sokolowskan16@gmail.com>
2020-08-24 14:40:48 +0200

    foundedPairs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Scanner lineScanner = new Scanner(scanner.nextLine());
         List<Integer> list = new ArrayList<>();
-        while (lineScanner.hasNextInt())
-        {
+        List<Integer> result = new ArrayList<>();
+        while (lineScanner.hasNextInt()) {
             list.add(lineScanner.nextInt());
         }
 
-        
+        for (int i = 0; i < list.size(); i++) {
+            int firstNumber = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                int secondNumber = list.get(j);
+                if (firstNumber + secondNumber == 13){
+                    if (firstNumber < secondNumber){
+                        result.add(firstNumber);
+                    }
+                    else{
+                        result.add(secondNumber);
+                    }
+                }
+            }
+
+        }
+        System.out.println(result);
     }
 }
+
